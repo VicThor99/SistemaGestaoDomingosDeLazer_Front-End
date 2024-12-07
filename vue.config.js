5ce47039cd52a9574de79be646ab3ddee6b0e762
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2024-12-07 14:56:29 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,19 +3,9 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     server: true, 
-    host: '127.0.0.1', 
-    port: 8443, 
     client: {
         webSocketURL: {
-            protocol: 'wss', 
-            hostname: '127.0.0.1',
-            port: 8443,
-        },
-    },
-    proxy: {
-        '/api': {
-            target: 'https://www.domingodelazer.click:8443',
-            changeOrigin: true,
+            protocol: 'wss'
         },
     },
     allowedHosts: [
