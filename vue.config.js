e424aa42c03608d720bc48357c0707a03b72ba42
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2024-12-07 14:36:21 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,25 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    https: true, // Serve the development server over HTTPS
+    host: 'www.domingodelazer.click', // Specify the host (default is localhost)
+    port: 8443, // Specify the port
+    client: {
+        webSocketURL: {
+            protocol: 'wss', // Use WSS for WebSocket
+            host: 'www.domingodelazer.click',
+            port: 8443,
+        },
+    },
+    proxy: {
+        '/api': {
+            target: 'https://www.domingodelazer.click:8443', // Proxy API requests to your backend
+            changeOrigin: true,
+        },
+    },
     allowedHosts: [
       'domingodelazer.click', 
       'www.domingodelazer.click' 
     ], 
-  }
+  },
 })
