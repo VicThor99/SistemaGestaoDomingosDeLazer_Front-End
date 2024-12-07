aec34ac88f172cd6ede3fdf32b8accaccdaa4951
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2024-12-07 13:34:27 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: [
+      'domingodelazer.click', // Domínio permitido
+      'www.domingodelazer.click' // Outro domínio permitido
+    ],
+    disableHostCheck: true, // Desabilita a verificação do cabeçalho Host
+  }
 })
