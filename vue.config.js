bf426dfca20c38e3a97ade930aa25276902169ca
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2025-01-09 20:33:07 -0300

    Config change

--- vue.config.js
+++ vue.config.js
@@ -10,14 +10,6 @@ module.exports = defineConfig({
             port:8443
         },
     },
-    proxy: {
-      '/api': {
-        target: 'https://api.domingodelazer.click',
-        port: 8443,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
     allowedHosts: [
       'domingodelazer.click', 
       'api.domingodelazer.click',
