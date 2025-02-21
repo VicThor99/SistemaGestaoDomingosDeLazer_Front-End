cb1466a2a38aff8b5db96df74f86db8cc962af64
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2025-02-21 15:40:51 -0300

    .

--- src/components/ContagemAlunos.vue
+++ src/components/ContagemAlunos.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="principal">
         <div id="contagemAlunos">
-            <h2 style="margin-left: 30px;">Contagem de alunos</h2>
+            <h2 style="text-align: center;">Contagem de alunos</h2>
             <hr style="opacity: 0.2; width: 99.86%;" />
-            <div style="height: 100%;">
+            <div style="height: 100%; margin-top: 10px;">
                 <div
-                    style="display: flex; justify-content: center;flex-direction: column;align-items: center;color:  #0b4d75;">
+                    style="display: flex; justify-content: center; flex-direction: column; align-items: center; color:  #0b4d75;">
                     <div style="border: 1px solid #0b4d75; width: 100%; display: flex; justify-content: space-around; flex-direction: row;" v-for="sala in salas">
                         <div style="border: 1px solid #0b4d75; padding: 5px; width: 25%;">
                             <h3>{{ sala.sala }}</h3>
