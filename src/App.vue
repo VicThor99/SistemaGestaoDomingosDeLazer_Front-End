9cd526ffb6dee1111557d7778bed8f02901cb149
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2025-02-23 17:40:08 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div v-show="!logado" id="divPrincipal">
+    <div v-if="!logado" id="divPrincipal">
       <div></div>
       <div id="login">
         <img class="img" alt="Logo da Seara" src="./assets/logo.png">
@@ -13,12 +13,12 @@
 
         <button id="logar" @click="submit">Logar</button>
 
-        <h2 class="aviso" v-show="erro !== null">{{ erro }}</h2>
+        <h2 class="aviso" v-if="erro !== null">{{ erro }}</h2>
       </div>
       <div></div>
     </div>
 
-    <div v-show="logado && !escolaEscolhida" id="divPrincipal">
+    <div v-if="logado && !escolaEscolhida" id="divPrincipal">
       <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
         <div style="display: flex; justify-content: space-around;">
           <div v-for="e in escolas" :key="e">
@@ -28,14 +28,14 @@
       </div>
     </div>
 
-    <div v-show="logado && escolaEscolhida" id="divPrincipal">
+    <div v-if="logado && escolaEscolhida" id="divPrincipal">
       <header id="header" style="font-size: 22px; box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);">
         <div style="width: 70%; height: 100%; display: flex; justify-content: flex-start;">
           <a @click="mudarPagina('TelaInicial')" class="headerImg"><img class="logoInicial" alt="Logo da Seara"
               src="./assets/logo.png"></a>
-          <a class="headerButton" v-show="admin == 'true'" @click="abrirOuFecharCadastros">Cadastros <i class="mdi mdi-menu-down"></i></a>
+          <a class="headerButton" v-if="admin == 'true'" @click="abrirOuFecharCadastros">Cadastros <i class="mdi mdi-menu-down"></i></a>
           <a class="headerButton" @click="abrirOuFecharPresencas">Presenças <i class="mdi mdi-menu-down"></i></a>
-          <a class="headerButton" v-show="admin == 'true'" @click="abrirOuFecharImpressoes">Impressões <i class="mdi mdi-menu-down"></i></a>
+          <a class="headerButton" v-if="admin == 'true'" @click="abrirOuFecharImpressoes">Impressões <i class="mdi mdi-menu-down"></i></a>
         </div>
         <div style="width: 30%;height: 100%; margin-top: 25px;display: flex; justify-content: flex-end;">
           <div style="width: 300px; text-align: right;">
@@ -46,7 +46,7 @@
       </header>
       <div
         style="margin: -15px -20px 0px 90px; border-radius: 0px 0px 5px 5px; width: 1020px; height: 45px; background-color: whitesmoke; display: flex; justify-content: start; align-self: start;"
-        v-show="cadastros">
+        v-if="cadastros">
         <a style="margin-top: 10px; margin-left: 15px; cursor: pointer;" @click="mudarPagina('Alunos')"><i
             class="mdi mdi-account-school"></i> Alunos</a>
         <a style="margin-top: 10px; margin-left: 15px; cursor: pointer;" @click="mudarPagina('Datas')"><i
@@ -64,7 +64,7 @@
       </div>
       <div
         style="margin: -15px -20px 0px 250px; border-radius: 0px 0px 5px 5px; width: 740px; height: 45px; background-color: whitesmoke; display: flex; justify-content: start; align-self: start;"
-        v-show="registroPresencas && admin == 'true'">
+        v-if="registroPresencas && admin == 'true'">
         <a style="margin-top: 10px; margin-left: 15px; cursor: pointer;" @click="mudarPagina('ContagemAlunos')"><i
             class="mdi mdi-list-box-outline"></i> Contagem de Alunos</a>
         <a style="margin-top: 10px; margin-left: 15px; cursor: pointer;" @click="mudarPagina('RegistroPresencas')"><i
@@ -76,13 +76,13 @@
       </div>
       <div
         style="margin: -15px -20px 0px 90px; border-radius: 0px 0px 5px 5px; width: 213px; height: 45px; background-color: whitesmoke; display: flex; justify-content: start; align-self: start;"
-        v-show="registroPresencas && admin != 'true'">
+        v-if="registroPresencas && admin != 'true'">
         <a style="margin-top: 10px; margin-left: 15px; cursor: pointer;" @click="mudarPagina('PresencaCelular')"><i
             class="mdi mdi-cellphone"></i> Registro (Celular)</a>
       </div>
       <div
         style="margin: -15px -20px 0px 410px; border-radius: 0px 0px 5px 5px; width: 491px; height: 45px; background-color: whitesmoke; display: flex; justify-content: start; align-self: start;"
-        v-show="impressoes">
+        v-if="impressoes">
         <a style="margin-top: 10px; margin-left: 15px; cursor: pointer;" @click="mudarPagina('ImpressaoMatriculas')"><i
             class="mdi mdi-file-document-edit"></i> Matrículas</a>
         <a style="margin-top: 10px; margin-left: 15px; cursor: pointer;" @click="mudarPagina('ImpressaoListas')"><i
@@ -95,7 +95,7 @@
 
       <div
         style="margin: -15px 15px 0px 0px; border-radius: 0px 0px 5px 5px;width: 430px; height: 45px; background-color: whitesmoke; display: flex; justify-content: end;  align-self: end;"
-        v-show="opcoes">
+        v-if="opcoes">
         <a style="margin-top: 10px; margin-right: 15px; cursor: pointer;" @click="download"><i
             class="mdi mdi-file-excel"></i> Exportar Excel</a>
         <a style="margin-top: 10px; margin-right: 15px; cursor: pointer;" @click="mudarPagina('ImportExcel')"><i
@@ -192,55 +192,53 @@ export default {
   },
   methods: {
     async submit() {
-      await axios.post('https://api.domingodelazer.click/api/auth', this.user, {
+      let res = await axios.post('https://api.domingodelazer.click/api/auth', this.user, {
         headers: {
           'Content-Type': 'application/json',
         }
-      })
-        .then(res => {
-          var date = new Date();
-          date.setUTCMinutes(date.getUTCMinutes() + 300);
-          cookies.set('token', 'Bearer ' + res.data.token, date);
-          cookies.set('admin', res.data.admin, date);
-          cookies.set('user_name', res.data.username, date);
-          this.username = cookies.get('user_name');
-          this.logado = cookies.get('token') != null;
-          this.admin = cookies.get('admin');
-          this.erro = null;
-          this.telaAtual = 'TelaInicial';
-          axios.get('https://api.domingodelazer.click/api/access/' + this.username, {
-            headers: {
-              'Authorization': 'Bearer ' + res.data.token
-            }
-          })
-            .then(res => {
-              if (res.data.length === 1) {
-                cookies.set('escolaEscolhida', res.data[0].idEscola, date);
-                this.escolaEscolhida = true;
-              } else {
-                this.escolas = res.data;
-              }
-            })
-            .catch(rej => {
-              console.log(rej)
-              cookies.remove('token');
-              cookies.remove('admin');
-              cookies.remove('user_name');
-              cookies.remove('escola');
-              this.logado = false;
-              this.erro = 'Algo de errado ocorreu, por favor tente novamente';
-            })
-        })
-        .catch(rej => {
-          console.log(rej)
-          cookies.remove('token');
-          cookies.remove('admin');
-          cookies.remove('user_name');
-          cookies.remove('escola');
-          this.escolaEscolhida = false;
-          this.logado = false;
-          this.erro = 'Usuário e senha não compatíveis';
-        })
+      }).catch(rej => {
+        console.log(rej)
+        cookies.remove('token');
+        cookies.remove('admin');
+        cookies.remove('user_name');
+        cookies.remove('escola');
+        this.escolaEscolhida = false;
+        this.logado = false;
+        this.erro = 'Usuário e senha não compatíveis';
+      });
+
+      var date = new Date();
+      date.setUTCMinutes(date.getUTCMinutes() + 300);
+      cookies.set('token', 'Bearer ' + res.data.token, date);
+      cookies.set('admin', res.data.admin, date);
+      cookies.set('user_name', res.data.username, date);
+      this.username = cookies.get('user_name');
+      this.logado = cookies.get('token') != null;
+      this.admin = cookies.get('admin');
+      this.erro = null;
+      this.telaAtual = 'TelaInicial';
+          
+      res = await axios.get('https://api.domingodelazer.click/api/access/' + this.username, {
+        headers: {
+          'Authorization': 'Bearer ' + res.data.token
+        }
+      }).catch(rej => {
+        console.log(rej)
+        cookies.remove('token');
+        cookies.remove('admin');
+        cookies.remove('user_name');
+        cookies.remove('escola');
+        this.logado = false;
+        this.erro = 'Algo de errado ocorreu, por favor tente novamente';
+      });
+      
+      if (res.data.length === 1) {
+        cookies.set('escolaEscolhida', res.data[0].idEscola, date);
+        this.escolaEscolhida = true;
+      } else {
+        this.escolas = res.data;
+      }
+        
     },
     download() {
       axios.get('https://api.domingodelazer.click/api/alunos/export/' + this.escola, {
@@ -261,14 +259,7 @@ export default {
           this.erro = rej;
         })
     },
-    alterarTimer(newTimer){
-        this.timer = newTimer;
-    },
     mudarPagina(tela) {
-      if(this.timer != null){
-        clearTimeout(this.timer);
-        this.timer = null;
-      }
       this.telaAtual = tela;
       this.opcoes = false;
       this.cadastros = false;
@@ -316,7 +307,7 @@ export default {
     },
     selecionarEscola(id) {
       var date = new Date();
-      date.setUTCMinutes(date.getUTCMinutes() + 60);
+      date.setUTCMinutes(date.getUTCMinutes() + 300);
       cookies.set('escolaEscolhida', id, date);
       this.escolaEscolhida = true;
     }
