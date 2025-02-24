c33ddcdcf5028cf9d5258a2ae194f87e7c932ca4
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2025-02-24 09:04:53 -0300

    .

--- src/components/TelaInicial.vue
+++ src/components/TelaInicial.vue
@@ -165,7 +165,7 @@
             <div style="width: 100%; margin: 15px; display: flex; flex-direction: column;">
                 <div style="display: flex; flex-direction: column; width: 100%; margin-bottom: 5px;" >
                     <div style="background-color: #E4EDF7; border-radius: 5px; padding: 10px;border: #0b4d75 1px solid;">
-                        <div id="chartContainer" style="height: 360px; width: 100%;" v-show="chart != null"></div>
+                        <div id="chartContainer" style="height: 360px; width: 100%;" v-if="chart != null"></div>
                     </div>
                 </div>
                 <div style="display: flex; justify-content: space-between; width: 100%; margin-bottom: 5px;">
