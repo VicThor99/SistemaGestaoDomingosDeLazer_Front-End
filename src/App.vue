3215721ce24efd60efda5cb8dfb33b3a7b35f6f8
Victor Oliveira <93015739+VicThor99@users.noreply.github.com>
2025-12-02 16:54:36 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -266,7 +266,7 @@ export default {
       })
         .then(res => {
           this.listaCriancas = [];
-          this.listaCriancas.push({ "codigo": "Código", "nome": "Nome", "idade": "Idade", "nascimento": "Data de Nascimento", "sexo": "Sexo", "turma": "Turma", "grupo": "Grupo", "sapato": "Calçado", "blusa": "Blusa", "calca": "Calça", "responsavel": "Responsável", "telefone": "Telefone" });
+          this.listaCriancas.push({ "codigo": "Código", "numeroSacolinha": "Número da Sacolinha", "nome": "Nome", "idade": "Idade", "nascimento": "Data de Nascimento", "sexo": "Sexo", "turma": "Turma", "grupo": "Grupo", "sapato": "Calçado", "blusa": "Blusa", "calca": "Calça", "responsavel": "Responsável", "telefone": "Telefone" });
           res.data.forEach((element) => this.listaCriancas.push(element));
           const data = utils.json_to_sheet(this.listaCriancas, { skipHeader: true });
           const wb = utils.book_new();
